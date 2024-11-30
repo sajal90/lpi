d70cc87bcb023e6e763d252ceef1097b30b5aa03
sajal90 <sajal2005gupta@gmail.com>
2024-11-30 22:56:23 +0530

    copy file with holes

---
+++ ch1/cp-holes.c
@@ -0,0 +1,45 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../headers/tlpi_hdr.h"
+
+#define MAX_READ 4096
+
+int main(int argc, char *argv[]) {
+	int source_fd, des_fd;
+	char buf[MAX_READ];
+	int buf_size;
+	
+
+	if(argc < 3) {
+		usageErr("%s [source] [destination]", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	errno = 0;
+	source_fd = open(argv[1], O_RDONLY);
+	if(source_fd == -1) {
+		fprintf(stderr, "Opening source failed: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	
+	errno = 0;
+	des_fd = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if(des_fd == -1) {
+		fprintf(stderr, "Opening des failed: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	while((buf_size = read(source_fd, buf, MAX_READ)) != 0) {
+		errno = 0;
+		if(write(des_fd, buf, buf_size) != buf_size) {
+			fprintf(stderr, "Error: %s", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	exit(EXIT_SUCCESS);
+}
---
+++ ch1/create-holes.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define OFFSET 100000
+
+int main() {
+	int fd;
+
+	errno = 0;
+	fd = open("file.hole", O_RDWR | O_CREAT,
+			S_IRUSR | S_IWUSR);
+	if(fd == -1) {
+		fprintf(stderr, "%s",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	errno = 0;
+	if((write(fd, "Hello, world", 12)) == -1) {
+		fprintf(stderr, "%s",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	
+	errno = 0;
+	if((lseek(fd, OFFSET, SEEK_CUR)) == -1) {
+		fprintf(stderr, "%s",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	errno = 0;
+	if((write(fd, "bye, world", 10)) == -1) {
+		fprintf(stderr, "%s",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
