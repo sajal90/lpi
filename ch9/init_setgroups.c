0ba2e795bf9959a50f761601bff1a192764abbbc
sajal90 <sajal2005gupta@gmail.com>
2024-12-07 22:55:44 +0530

    ch9

---
+++ ch9/init_setgroups.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+	
+int my_setgroups(size_t gidsetsize, const gid_t *grouplist);
+int my_initgroups(const char *user, gid_t group);
+
+int main(int argc, char *argv[]) {
+
+}
+
+int my_setgroups(size_t gidsetsize, const gid_t *grouplist) {
+
+	return 0;
+}
+
+int my_initgroups(const char *user, gid_t group) {
+	
+	return 0;
+}
